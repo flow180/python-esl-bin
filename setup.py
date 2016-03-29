d815c3a0d21e1858ac4f528d19558276fabb866d
Charlie Wolf <charlie@flow180.com>
2016-03-29 01:00:23 -0500

    so copying code

--- setup.py
+++ setup.py
@@ -1,14 +1,41 @@
+import logging
+import os
 import sys
+from glob import glob
 from setuptools import setup
+from distutils.command.build import build
+from setuptools.command.easy_install import easy_install
 
 assert sys.version_info[0:2] == (3,5)
+
+
+class BuildWithSO(build):
+    def run(self):
+        build.run(self)
+        for path in glob(os.path.join(os.path.dirname(__file__),  '*.so')):
+            dest = os.path.join(self.build_lib, os.path.basename(path))
+            logging.info("Copying %s to %s." % (path, dest))
+            self.copy_file(path, dest)
+
+class EasyInstallWithSO(easy_install):
+    def run(self):
+        easy_install.run(self)
+        for path in glob(os.path.join(os.path.dirname(__file__),  '*.so')):
+            dest = os.path.join(self.install_dir, os.path.basename(path))
+            logging.info("Copying %s to %s." % (path, dest))
+            self.copy_file(path, dest)
 
 setup(
     name = "ESL",
     version = "1.6.6",
     py_modules=['ESL'],
     zip_safe=False,
-    data_files = [('', ['_ESL.so'])],
+    include_package_data=True,
+    package_data={'':['_ESL.so']},
+    cmdclass={
+        'build': BuildWithSO,
+        'easy_install': EasyInstallWithSO,
+    }
 )
 
 
